93eb3b2e9743b7ecd7f4332a24f55e68884d7178
Hayden Cook <38864544+hcook95@users.noreply.github.com>
2026-03-04 15:38:28 -0500

    Added installation steps for HELICS.
    Updated comments for clarity and added installation steps for HELICS.

--- cps_pi_setup.sh
+++ cps_pi_setup.sh
@@ -49,9 +49,8 @@ sudo systemctl daemon-reload
 sudo systemctl enable novnc
 sudo systemctl start novnc
 
-# Install ROS2 Kilted Kaiju on Raspberry Pi OS--Debian Trixie 13.3 (Based off of instructions found in https://forums.raspberrypi.com/viewtopic.php?t=361746)
+# Build and install ROS2 Kilted Kaiju on Raspberry Pi OS--Debian Trixie 13.3 (Based off of instructions found in https://forums.raspberrypi.com/viewtopic.php?t=361746)
 sudo apt install -y git colcon python3-rosdep2 vcstool wget python3-flake8-docstrings python3-pip python3-pytest-cov python3-flake8-blind-except python3-flake8-builtins python3-flake8-class-newline python3-flake8-comprehensions python3-flake8-deprecated python3-flake8-import-order python3-flake8-quotes python3-pytest-repeat python3-pytest-rerunfailures python3-vcstools libx11-dev libxrandr-dev libasio-dev libtinyxml2-dev lttng-tools
-cd
 mkdir -p ~/ros2_kilted/src
 cd ~/ros2_kilted
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/kilted/ros2.repos src
@@ -61,7 +60,21 @@ sudo rosdep init
 rosdep update
 rosdep install --from-paths src --ignore-src --rosdistro kilted -y --skip-keys "fastcdr rti-connext-dds-7.3.0 urdfdom_headers python3-vcstool python3-pyqt5 python3-sip python3-qt5-bindings"
 colcon build --symlink-install
+printf "\nalias ros2_local_setup=\"source /home/pi/ros2_kilted/install/local_setup.bash\"\nexport PATH=\"\$PATH:/home/pi/ros2_kilted/install/bin\"\n" >> ~/.bashrc # create setup alias and ros2 bin to PATH
 rm -rf logs/* build/ src/ # Clean up build files
+
+# Build and install HELICS
+sudo apt install -y libzmq3-dev 
+cd
+git clone https://github.com/GMLC-TDC/HELICS
+git checkout v3.6.1
+cd HELICS/
+git checkout v3.6.1
+mkdir build
+cd build 
+cmake ..
+make -j4
+printf "\nexport PATH=\"\$PATH:/home/pi/HELICS/build/bin\"./\n" >> ~/.bashrc # HELICS bin to PATH in .bashrc
 
 # Clean up home dir and create backup image at /media/backup.img
 rm ~/.cache/* ~/.vscode_server/ ~/*.log ~/.copilot/ ~/.config/chromium/ ~/.config/mozilla
