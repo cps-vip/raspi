88cd410698efd4ce6bdcadf35b634c09b73db862
Hayden Cook <38864544+hcook95@users.noreply.github.com>
2026-03-13 13:37:56 -0400

    Enable predictable interface names

--- cps_pi_setup.sh
+++ cps_pi_setup.sh
@@ -1,6 +1,7 @@
 # Configure some Pi settings
 sudo raspi-config nonint do_browser firefox
 sudo raspi-config nonint do_wayland W1
+sudo raspi-config nonint do_net_names 0
 sudo raspi-config nonint do_vnc 0
 sudo raspi-config nonint do_vnc_resolution "1280x720"
 
@@ -8,8 +9,8 @@ sudo raspi-config nonint do_vnc_resolution "1280x720"
 sudo apt install xrdp vim htop curl git screen x11-apps kate
 
 #S etup static IP
-sudo nmcli connection add   type ethernet ifname eth0 con-name eth0-static   ipv4.method manual   ipv4.addresses 192.168.2.2/24   ipv4.gateway 192.168.2.1   ipv4.dns "8.8.8.8 1.1.1.1"   autoconnect yes
-sudo nmcli connection up eth0-static
+sudo nmcli connection add   type ethernet ifname end0 con-name end0-static   ipv4.method manual   ipv4.addresses 192.168.2.2/24   ipv4.gateway 192.168.2.1   ipv4.dns "8.8.8.8 1.1.1.1"   autoconnect yes
+sudo nmcli connection up end0-static
 
 # Revert to legacy VNC authentication/password
 printf 'Encryption=PreferOn\nAuthentication=VncAuth\n' | sudo tee -a /root/.vnc/config.d/vncserver-x11
